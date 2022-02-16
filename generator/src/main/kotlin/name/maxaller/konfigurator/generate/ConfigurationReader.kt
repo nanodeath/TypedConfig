0b921609221c53274b43daf86a331855f68b0bf6
Max Aller <max@maxaller.name>
2022-02-15 20:44:45 -0800

    Support generating nested configuration structures.
    Only supports one level of nesting, but it's a start.

--- generator/src/main/kotlin/name/maxaller/konfigurator/generate/ConfigurationReader.kt
+++ generator/src/main/kotlin/name/maxaller/konfigurator/generate/ConfigurationReader.kt
@@ -1,5 +1,6 @@
 package name.maxaller.konfigurator.generate
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.dataformat.toml.TomlMapper
 import com.squareup.kotlinpoet.*
@@ -10,31 +11,10 @@ class ConfigurationReader {
 
     fun readFile(file: File): FileSpec {
         val node = tomlMapper.readTree(file)
-        val configs: List<ConfigDef> = node.fields().asSequence()
-            .filter { (_, v) -> v.isObject }
-            .map { (key, value) ->
-                require(key.isNotBlank()) { "Key cannot be empty or blank, was `${key}` in $file" }
-                when (val datatype = value.path("datatype").textValue()) {
-                    "int" -> {
-                        val defaultValue: Int? = value.get("default")?.intValue()
-                        val constraints: List<String>? =
-                            (value.get("constraints") as ArrayNode?)?.map { it.textValue() }
+        val configs: List<ConfigDef> = parseNode(node, file)
 
-                        IntConfigDef(key, defaultValue, constraints.orEmpty())
-                    }
-                    "str" -> {
-                        val defaultValue: String? = value.get("default")?.textValue()
-                        val constraints: List<String>? =
-                            (value.get("constraints") as ArrayNode?)?.map { it.textValue() }
-
-                        StringConfigDef(key, defaultValue, constraints.orEmpty())
-                    }
-                    else -> throw IllegalArgumentException("Unsupported datatype: `${datatype}` in $file")
-                }
-            }
-            .toList()
-
-        val className = node.get("class").textValue()
+        val packageName = node.get("package").textValue()
+        val className = ClassName(packageName, node.get("class").textValue())
         val configClass = TypeSpec.classBuilder(className)
         configClass.primaryConstructor(
             FunSpec.constructorBuilder()
@@ -47,9 +27,12 @@ class ConfigurationReader {
                 .addModifiers(KModifier.PRIVATE)
                 .build()
         )
+        val innerClasses = mutableMapOf<InnerTypeSpec, TypeSpec.Builder>()
         for (configDef in configs) {
             when (configDef) {
                 is IntConfigDef -> {
+                    val classToUpdate = getClassToUpdate(configDef.key, innerClasses, configClass)
+
                     val constraints = configDef.constraints.map {
                         when (it) {
                             "nonnegative" -> nonNegativeIntClassName
@@ -57,8 +40,8 @@ class ConfigurationReader {
                         }
                     }
                     val constraintsInterpolation = constraints.joinToString(", ") { "%T" }
-                    configClass.addProperty(
-                        PropertySpec.builder(configDef.key, Int::class)
+                    classToUpdate.addProperty(
+                        PropertySpec.builder(configDef.key.substringAfterLast('.'), Int::class)
                             .delegate(
                                 "%T(%S, %N, %L, listOf($constraintsInterpolation))",
                                 intKeyClassName,
@@ -71,6 +54,8 @@ class ConfigurationReader {
                     )
                 }
                 is StringConfigDef -> {
+                    val classToUpdate = getClassToUpdate(configDef.key, innerClasses, configClass)
+
                     val constraints = configDef.constraints.map {
                         when (it) {
                             "notblank" -> notBlankStringClassName
@@ -78,8 +63,8 @@ class ConfigurationReader {
                         }
                     }
                     val constraintsInterpolation = constraints.joinToString(", ") { "%T" }
-                    configClass.addProperty(
-                        PropertySpec.builder(configDef.key, String::class)
+                    classToUpdate.addProperty(
+                        PropertySpec.builder(configDef.key.substringAfterLast('.'), String::class)
                             .delegate(
                                 "%T(%S, %N, %S, listOf($constraintsInterpolation))",
                                 stringKeyClassName,
@@ -93,9 +78,75 @@ class ConfigurationReader {
                 }
             }
         }
-        val packageName = node.get("package").textValue()
-        return FileSpec.builder(packageName, className)
+        for ((innerTypeSpec, innerTypeBuilder) in innerClasses) {
+            configClass.addProperty(
+                PropertySpec.builder(innerTypeSpec.fieldName, className.nestedClass(innerTypeSpec.className))
+                    .initializer("%T()", className.nestedClass(innerTypeSpec.className))
+                    .build()
+            )
+            configClass.addType(innerTypeBuilder.build())
+        }
+        return FileSpec.builder(packageName, className.simpleName)
             .addType(configClass.build())
             .build()
     }
+
+    private fun getClassToUpdate(
+        key: String,
+        innerClasses: MutableMap<InnerTypeSpec, TypeSpec.Builder>,
+        configClass: TypeSpec.Builder
+    ) = if ('.' in key) {
+        // FIXME only supports one level of nesting for now
+        val field = key.substringBeforeLast('.').split('.').single()
+        val innerName = field.replaceFirstChar { it.uppercase() }
+        innerClasses.getOrPut(InnerTypeSpec(field, innerName)) {
+            TypeSpec.classBuilder(innerName)
+                .addModifiers(KModifier.INNER)
+                .primaryConstructor(
+                    FunSpec.constructorBuilder().addModifiers(KModifier.INTERNAL).build()
+                )
+        }
+    } else {
+        configClass
+    }
+
+    private data class InnerTypeSpec(val fieldName: String, val className: String)
+
+    private fun parseNode(
+        node: JsonNode,
+        file: File,
+        precedingKey: List<String> = emptyList()
+    ): List<ConfigDef> = node.fields().asSequence()
+        .filter { (_, v) -> v.isObject }
+        .flatMap { (key, value) ->
+            require(key.isNotBlank()) { "Key cannot be empty or blank, was `${key}` in $file" }
+
+            val fullKey = if (precedingKey.isEmpty()) key else "${precedingKey.joinToString(".")}.$key"
+
+            when (val datatype = value.path("datatype").textValue()) {
+                "int" -> {
+                    val defaultValue: Int? = value.get("default")?.intValue()
+                    val constraints: List<String>? =
+                        (value.get("constraints") as ArrayNode?)?.map { it.textValue() }
+
+                    sequenceOf(IntConfigDef(fullKey, defaultValue, constraints.orEmpty()))
+                }
+                "str" -> {
+                    val defaultValue: String? = value.get("default")?.textValue()
+                    val constraints: List<String>? =
+                        (value.get("constraints") as ArrayNode?)?.map { it.textValue() }
+
+                    sequenceOf(StringConfigDef(fullKey, defaultValue, constraints.orEmpty()))
+                }
+                null -> {
+                    if (value.isObject) {
+                        parseNode(value, file, precedingKey + listOf(key)).asSequence()
+                    } else {
+                        emptySequence()
+                    }
+                }
+                else -> throw IllegalArgumentException("Unsupported datatype: `${datatype}` in $file")
+            }
+        }
+        .toList()
 }
