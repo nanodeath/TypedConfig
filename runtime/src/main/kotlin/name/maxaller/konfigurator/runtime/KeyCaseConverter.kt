d3462772165086a1da6567bc19d92ffbeea9868a
Max Aller <max@maxaller.name>
2022-02-16 08:34:26 -0800

    Update KeyCaseConverter.kt to also handle dots.

--- runtime/src/main/kotlin/name/maxaller/konfigurator/runtime/KeyCaseConverter.kt
+++ runtime/src/main/kotlin/name/maxaller/konfigurator/runtime/KeyCaseConverter.kt
@@ -5,9 +5,11 @@ internal fun String.camelToUpperSnake(): String {
     return buildString((this.length * 1.25).toInt()) {
         for (idx in str.indices) {
             val char = str[idx]
-            append(char.uppercase())
+            if (char != '.') {
+                append(char.uppercase())
+            }
             val next = str.getOrNull(idx + 1) ?: break
-            if (Character.isUpperCase(next) && Character.isLowerCase(char)) {
+            if (Character.isUpperCase(next) && Character.isLowerCase(char) || char == '.') {
                 append('_')
             }
         }
--- runtime/src/test/kotlin/name/maxaller/konfigurator/runtime/CaseConverterTest.kt
+++ runtime/src/test/kotlin/name/maxaller/konfigurator/runtime/CaseConverterTest.kt
@@ -13,4 +13,14 @@ internal class CaseConverterTest {
     fun upperCamelCase() {
         "UpperCamelCase".camelToUpperSnake() shouldBe "UPPER_CAMEL_CASE"
     }
+
+    @Test
+    fun lowerCamelCaseWithDots() {
+        "app.port".camelToUpperSnake() shouldBe "APP_PORT"
+    }
+
+    @Test
+    fun lowerCamelCaseWordsWithDots() {
+        "app.securePort".camelToUpperSnake() shouldBe "APP_SECURE_PORT"
+    }
 }
