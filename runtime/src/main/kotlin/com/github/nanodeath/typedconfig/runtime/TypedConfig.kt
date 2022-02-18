5a576f389669382fedec12063bcd7626146a8b1f
Max Aller <max@maxaller.name>
2022-02-18 07:54:34 -0800

    Rewrite TypedConfig to use new WritableOnceDelegate for defaultSource.
    Previously too much of the write-once logic was directly in TypedConfig.
    
    Closes #26.

--- runtime/src/main/kotlin/com/github/nanodeath/typedconfig/runtime/TypedConfig.kt
+++ runtime/src/main/kotlin/com/github/nanodeath/typedconfig/runtime/TypedConfig.kt
@@ -4,17 +4,5 @@ import com.github.nanodeath.typedconfig.runtime.source.EnvSource
 import com.github.nanodeath.typedconfig.runtime.source.Source
 
 object TypedConfig {
-    private var sourceInitialized = false
-
-    var defaultSource: Source = EnvSource()
-        @Synchronized get
-        @Synchronized
-        set(value) {
-            if (!sourceInitialized) {
-                field = value
-                sourceInitialized = true
-            } else {
-                throw IllegalStateException("defaultSource has already been initialized!")
-            }
-        }
+    var defaultSource: Source by WriteOnceDelegate(EnvSource(), "defaultSource has already been initialized")
 }
