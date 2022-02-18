ae4da2d19863aff25d4f137344c81d6a2a9ec38d
Max Aller <max@maxaller.name>
2022-02-17 19:04:15 -0800

    Introduce JsonSource, along with runtime-interfaces and runtime-source-json modules.
    Closes #16.

--- runtime-source-json/build.gradle.kts
+++ runtime-source-json/build.gradle.kts
@@ -3,7 +3,9 @@ plugins {
 }
 
 dependencies {
-    api(project(":runtime-interfaces"))
+    implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.jr:jackson-jr-objects:2.13.1")
+    implementation(project(":runtime-interfaces"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("io.kotest:kotest-assertions-core:5.1.0")
     testImplementation("io.mockk:mockk:1.12.2")
---
+++ runtime-source-json/src/main/kotlin/com/github/nanodeath/typedconfig/runtime/source/JsonSource.kt
@@ -0,0 +1,20 @@
+package com.github.nanodeath.typedconfig.runtime.source
+
+import com.fasterxml.jackson.jr.ob.JSON
+import java.io.File
+
+class JsonSource internal constructor(private val document: Map<String, Any>) : Source {
+    constructor(file: File) : this(JSON.std.mapFrom(file.bufferedReader()))
+
+    override fun getInt(key: String): Int? = lookup(key) as? Int
+
+    override fun getString(key: String): String? = lookup(key) as? String
+
+    override fun getDouble(key: String): Double? = lookup(key) as? Double
+
+    private fun lookup(key: String) = key
+        .splitToSequence('.')
+        .fold<String, Any?>(document) { current, subkey ->
+            (current as? Map<*, *>)?.get(subkey)
+        }
+}
