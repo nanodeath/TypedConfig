e3925074f99dcfd6e47b7215cdc556e323151e32
Max Aller <max@maxaller.name>
2022-02-19 10:54:24 -0800

    Add simple Gradle sample application.
    Closes #35.

---
+++ samples/simple-gradle/src/main/kotlin/Application.kt
@@ -0,0 +1,8 @@
+import com.example.GeneratedConfig
+
+fun main() {
+    val config = GeneratedConfig.default()
+    val greeting = config.greeting
+    println(greeting)
+    println("Configuration was compiled and loaded successfully!")
+}
