e26412a586636961f81659ac5fd2cc66be6dd106
Max Aller <max@maxaller.name>
2022-02-19 15:53:17 -0800

    You can now configure multiple config files with the plugin.

--- gradle-plugin/src/main/kotlin/com/github/nanodeath/typedconfig/TypedConfigPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/nanodeath/typedconfig/TypedConfigPlugin.kt
@@ -1,16 +1,32 @@
 package com.github.nanodeath.typedconfig
 
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.plugins.ide.idea.IdeaPlugin
 import org.gradle.plugins.ide.idea.model.IdeaModel
 
+interface TypedConfigFile {
+    val name: String
+    val file: RegularFileProperty
+}
+
+interface TypedConfigExtension {
+    val configs: NamedDomainObjectContainer<TypedConfigFile>
+}
+
+const val DEFAULT_CONFIG_FILENAME = "config.tc.toml"
+
 class TypedConfigPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        val extension = project.extensions.create("typedConfig", TypedConfigExtension::class.java)
+
         val configuration = project.configurations.create("typedConfigGenerator")
         project.dependencies.add(configuration.name, "com.github.nanodeath.typedconfig:generator:1.0-SNAPSHOT")
         project.plugins.withType(JavaPlugin::class.java) {
@@ -24,17 +40,41 @@ class TypedConfigPlugin : Plugin<Project> {
                 module.generatedSourceDirs.add(generatedSourcesDir)
             }
         }
-        project.tasks.register("generateTypedConfigClasses", JavaExec::class.java) { t ->
-            t.group = "build"
-            val inputFile = "config.tc.toml"
-            t.inputs.file(inputFile)
-            t.outputs.dir(generatedSourcesDir)
-            t.classpath = configuration
-            t.mainClass.set("com.github.nanodeath.typedconfig.generate.Generator")
-            t.args(inputFile, generatedSourcesDir)
-        }
         val sourceSets = project.extensions.getByType(SourceSetContainer::class.java)
         val javaSourceSet = sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).java
         javaSourceSet.srcDir(generatedSourcesDir)
+
+        project.afterEvaluate {
+            val configs = extension.configs.takeUnless { it.isEmpty() }?.asMap ?: mapOf(
+                "default" to buildDefaultTypedConfigFile(project)
+            )
+            val tasks: List<TaskProvider<JavaExec>> = configs.map { (name, config) ->
+                val taskName = "generate${name.replaceFirstChar { it.uppercase() }}TypedConfig"
+                project.tasks.register(taskName, JavaExec::class.java) { t ->
+                    t.group = "typed config"
+                    val inputFile = config.file.get()
+                    t.inputs.file(inputFile)
+                    t.outputs.dir(generatedSourcesDir)
+                    t.classpath = configuration
+                    t.mainClass.set("com.github.nanodeath.typedconfig.generate.Generator")
+                    t.args(inputFile, generatedSourcesDir)
+                }
+            }
+            project.tasks.register("generateTypedConfigs") {
+                it.group = "typed config"
+                it.dependsOn(tasks)
+            }
+        }
+    }
+
+    private fun buildDefaultTypedConfigFile(
+        project: Project
+    ): TypedConfigFile {
+        val objects = project.objects
+        return object : TypedConfigFile {
+            override val name = "default"
+            override val file
+                get() = objects.fileProperty().value(project.layout.projectDirectory.file(DEFAULT_CONFIG_FILENAME))
+        }
     }
 }
